c3099939dff9681b89924207d568aa449dfb584f
Aditya Pradita <pradita397@gmail.com>
2025-11-26 14:54:07 +0700

    Reducing padding in hero section to adjust height

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -2,7 +2,7 @@ const HeroSection = () => {
   return (
     <div className="bg-gray-900">
       <section className="relative isolate px-4 pt-14 lg:px-8">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-28 sm:py-36 lg:py-44">
           <div className="text-center">
             <h1 className="text-3xl font-semibold text-white sm:text-5xl">
               Welcome to Cifera Shop!
